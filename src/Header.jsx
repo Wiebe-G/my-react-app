72b9aaf794c569da32f667910d2806e592bd4bd2
Wiebe Gouma <goumawiebe1234@proton.me>
2026-03-06 15:32:16 +0100

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Mijn eerste react applicatie</h1>
+            <nav>
+                <ul>
+                    <li>Test 1</li>
+                    <li>Test 2</li>
+                    <li>Test 3</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
